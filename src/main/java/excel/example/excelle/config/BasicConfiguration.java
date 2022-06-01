0a9fad2002f2b757d2bfa329742306747e77b911
moutendy <christophermoutendy@gmail.com>
2022-06-01 04:40:40 +0200

    configiration de spring securité

--- src/main/java/excel/example/excelle/config/BasicConfiguration.java
+++ src/main/java/excel/example/excelle/config/BasicConfiguration.java
@@ -1,5 +1,8 @@
 package excel.example.excelle.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,34 +10,46 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpMethod;
+
 
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
 public class BasicConfiguration extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private DataSource dataSource;
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 	
+	
+		
+		
+		
 		PasswordEncoder encoder = 
 		          PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth
-        .inMemoryAuthentication()
-        .withUser("user")
-        .password(encoder.encode("password"))
-        .roles("USER")
-        .and()
-        .withUser("admin")
-        .password(encoder.encode("admin"))
-        .roles("USER", "ADMIN");
+		auth.jdbcAuthentication()                
+	    .dataSource(dataSource)                
+	    .usersByUsernameQuery("select username as principal, password as credentials, true from users where username = ?")               
+	    .authoritiesByUsernameQuery("select username as principal, authority as role from authorities where username = ?")                
+	    .passwordEncoder(encoder);
 	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		   http
-	          .authorizeRequests()
-	          .anyRequest()
-	          .authenticated()
-	          .and()
-	          .httpBasic();
+		 //HTTP Basic authentication
+           .httpBasic()
+           .and()
+           .authorizeRequests()
+           .antMatchers(HttpMethod.GET, "/listeUsers/**").hasRole("USER")
+           .antMatchers(HttpMethod.POST, "/books").hasRole("ADMIN")
+           .antMatchers(HttpMethod.PUT, "/books/**").hasRole("ADMIN")
+           .antMatchers(HttpMethod.PATCH, "/books/**").hasRole("ADMIN")
+           .antMatchers(HttpMethod.DELETE, "/books/**").hasRole("ADMIN")
+           .and()
+           .csrf().disable()
+           .formLogin().disable();
 	}
+	
 }
